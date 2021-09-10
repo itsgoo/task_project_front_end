edf82f05dd86f0671fceb23c4a3bd2e31d848e53
devmashine <kurdalin.hi@gmail.com>
2021-09-10 15:49:57 +0300

    debug link to server side2

--- src/views/app/AddTasks.js
+++ src/views/app/AddTasks.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import inkToServerSide from './LinkToServerSide';
+import linkToServerSide from './LinkToServerSide';
 
 
 const AddTasks = ({fetchTasks}) => {
